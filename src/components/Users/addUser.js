376fafb841a45ea8e97b7c992e2f9bb058d7ede0
Ezz-Ashraf <ezzashraf056@gmail.com>
2023-04-03 23:12:29 +0200

    add state

--- src/components/Users/addUser.js
+++ src/components/Users/addUser.js
@@ -1,12 +1,26 @@
-import React from "react";
-
+import React ,{useState} from "react";
+import Button from "../UI/Button";
 import classes from './adduser.module.css'
 import Card from "../UI/Card";
 const AddUser = props => {
 
+    const [enteredUserName , setEnteredUserName] = useState('')
+    const [enteredAge , setEnteredAge] = useState(0)
     const addUserHandler = (event) =>{
+
         event.preventDefault();
+        console.log("Entered user is "+enteredUserName +" and his age is " + enteredAge)
+        setEnteredAge(0)
+        setEnteredUserName('')
         return true
+    }
+
+    const userNameChange = (event) =>{
+        setEnteredUserName(event.target.value);
+    }
+
+    const ageChange = (event) =>{
+        setEnteredAge(event.target.value);
     }
 
     return (
@@ -15,13 +29,13 @@ const AddUser = props => {
             <label  htmlFor="userName">
             UserName
             </label>
-            <input  id="userName" type="text"/>
+            <input  id="userName" type="text" onChange={userNameChange} value={enteredUserName}/>
 
             <label  htmlFor="age">
             Age
             </label>
-            <input  id="age" type="number"/>
-            <button type="submit">Add User</button>
+            <input  id="age" type="number" onChange={ageChange} value={enteredAge}/>
+            <Button type="submit">Add User</Button>
         </form>
         </Card>
     );
